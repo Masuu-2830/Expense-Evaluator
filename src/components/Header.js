0ec5027a37de2fc70a9c89902175daf288c06e41
Masuu-2105 <masoodmuhammad365@gmail.com>
2020-06-09 15:05:33 +0530

    Uploading to github

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <h2 className="header">
+            <span className="sp1">Expense </span><span className="sp2">Evaluator</span>
+        </h2>
+    )
+}
